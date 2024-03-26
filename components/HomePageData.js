1b8a780694f5fc539008817bbdf1ef743ea08178
krishiiitp <98936053+krishiiitp@users.noreply.github.com>
2024-03-26 23:39:16 +0530

    initial commit

---
+++ components/HomePageData.js
@@ -0,0 +1,28 @@
+export let AboutUsData = [
+    {
+        headline : "Tailored Solution",
+        content : "We deliver tailor-made, affordable yet elegant events, understanding and fulfilling our clients' unique requirements."
+    },
+    {
+        headline : "Creative Team",
+        content : "Our innovative event organization is led by a friendly team of youthful, imaginative minds, meticulously attentive to detail."
+    },
+    {
+        headline : "Perfect Event",
+        content : "Our speciality is creating amazing events that are professionally executed and customised to your passions and interests."
+    }
+]
+export let ServicesData = [
+    {
+        headline : "Wedding Event",
+        content : "Welcome to our world of timeless elegance and unforgettable moments. We understand that your wedding day is a reflection of your love story, unique and cherished. Our team of experienced wedding planners is dedicated to crafting bespoke experiences that exceed your every expectation. From intimate ceremonies to grand celebrations, we offer personalized services that cater to your every need.Whether you envision a romantic garden affair, a luxurious ...",
+        image : "/wedding_event.png",
+        side : true
+    },
+    {
+        headline : "Corporate Event",
+        content : "Welcome to Mann Events, where we specialize in creating impactful corporate events that leave a lasting impression. With a blend of creativity and professionalism, our experienced team is dedicated to bringing your vision to life. From dynamic conferences and engaging seminars to elegant gala dinners and team-building retreats, we tailor our services to meet your company's objectives and exceed your expectations. With meticulous attention to detail, we handle every ...",
+        image : "/corporate_event.jpg",
+        side : false
+    }
+]
