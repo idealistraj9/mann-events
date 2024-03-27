1d468453d7553c95fab3ce2084f7f4d23c05a2f6
krishiiitp <98936053+krishiiitp@users.noreply.github.com>
2024-03-27 14:37:19 +0530

    updating home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
           <h1 className="text-4xl font-extrabold mb-5 mt-5" style={someStyle}>We tailor our event services to your needs, ensuring meticulous management from start to finish.</h1>
           <h1 style={someStyle}>At Mann Events, we blend artistry and precision to deliver fulfilling experiences tailored to our clients' needs. Since 2018, we've catered to corporate, individual, and charitable sectors, specializaing in a range of events from weddings to corporate functions. With expertise in pre-wedding shoots and destination weddings, we ensure every detail is meticulously executed for a memorable and engaging experience.</h1>
         </div>
-        <div className="flex w-3/4 gap-10">
+        <div className="flex w-4/5 gap-10">
           {AboutUsData.map((item,index)=>(
               <div className="flex flex-col w-1/3 border border-pink-600 border-4 rounded-lg px-5 py-5 hover:bg-pink-600 hover:text-white">
                 <div className="w-1/5 mx-auto bg-gray-300 py-7 justify-center items-center text-center rounded-lg">
@@ -56,16 +56,16 @@ export default function Home() {
                 </div>
                 <div className="w-1/2 flex flex-col gap-y-6 justify-left items-left text-left px-14">
                   <h1 style={someStyle} className="text-4xl font-extrabold mb-4">{item.headline}</h1>
-                  <h1 style={someOtherStyle}>{item.content}</h1>
-                  <button className="text-pink-600 px-4 py-4 border border-pink-600 mt-6 rounded-lg w-1/4 font-bold">Know More</button>
+                  <h1>{item.content}</h1>
+                  <button className="text-pink-600 px-4 py-4 border border-pink-600 mt-6 rounded-lg w-1/4 font-bold hover:bg-pink-600 hover:text-white">Know More</button>
                 </div>
               </div>
               ) : (
                 <div className="flex w-full">
                 <div className="w-1/2 flex flex-col gap-y-6 px-14 justify-start text-left items-left">
                   <h1 style={someStyle} className="text-4xl font-extrabold mb-4">{item.headline}</h1>
-                  <h1 style={someOtherStyle}>{item.content}</h1>
-                  <button className="text-pink-600 px-4 py-4 border border-pink-600 mt-6 rounded-lg w-1/4 font-bold">Know More</button>
+                  <h1>{item.content}</h1>
+                  <button className="text-pink-600 px-4 py-4 border border-pink-600 mt-6 rounded-lg w-1/4 font-bold hover:bg-pink-600 hover:text-white">Know More</button>
                 </div>
                 <div className="w-1/2 flex items-center justify-center">
                   <Image 
@@ -92,7 +92,16 @@ export default function Home() {
                 <h1 style={someStyle} className="text-4xl text-pink-600">Our execution.</h1>
             </div>
         </div>
-    </div>
+      </div>
+      <div className="flex w-11/12 bg-pink-600 px-12 py-16 rounded-lg">
+          <div className="flex flex-col w-3/5 gap-y-12 justify-start items-left text-left">
+            <h1 style={someStyle} className="text-white text-4xl text-bold">Let us bring your event to life</h1>
+            <h1 style={someStyle} className="text-white text-2xl">Our skilled event planners are dedicated to creating unforgettable experiences for you</h1>
+          </div>
+          <div className="w-2/5 flex justify-center items-center text-center">
+            <button style={someStyle} className="bg-white px-12 py-4 text-pink-600 text-bold text-2xl rounded-lg">Get Started Today</button>
+          </div>
+      </div>
     </div>
   );
 }
